50b5d3f3cdbbbc2d70d99633d34f12ee964c57e5
Zach Schwartz <zischwartz@gmail.com>
2017-12-21 11:24:56 -0500

    try catch for cloneable

--- src/magicSandbox.js
+++ src/magicSandbox.js
@@ -262,7 +262,12 @@ export default function (template, files, sandbox_id) {
     }
     window.console_log_native = window.console.log
     window.console.log = function(){
-      window.parent.postMessage({type: "console-log", message: arguments, sandbox_id:${sandbox_id}}, "` + window.location.origin + `")
+      try {
+        window.parent.postMessage({type: "console-log", message: arguments, sandbox_id:${sandbox_id}}, "` + window.location.origin + `")
+      }
+      catch(err){
+        window.parent.postMessage({type: "console-log", message: 'CONSOLE-ERROR', sandbox_id:${sandbox_id}}, "` + window.location.origin + `")        
+      }
       window.console_log_native(arguments)
     }
   })()</script>` + template;
