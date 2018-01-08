ae5583c031531e424a78884bbbfd4e59db0d811a
Zach Schwartz <zischwartz@gmail.com>
2018-01-08 13:04:24 -0500

    oops

--- src/magicSandbox.js
+++ src/magicSandbox.js
@@ -257,8 +257,7 @@ export default function (template, files, supress_console_log=false) {
   //XXX and then added by zach, also create a message for console.logs
   template = `<script>(function(){
     window.onerror = function(msg, url, lineNumber, colNumber) {
-      window.parent.postMessage({type: "runtime-error", lineNumber:lineNumber, colNumber:colNumber, mLines:lines, message:msg}, "` + window.location.origin + `")
-      //console.debug('blockbuilder editor error on line: ' + (lineNumber-` + lines + `))
+      window.parent.postMessage({type: "runtime-error", lineNumber:lineNumber, colNumber:colNumber, mLines:${lines}, message:msg}, "` + window.location.origin + `")
     }
     window.console_log_native = window.console.log
     window.console.log = function(){
